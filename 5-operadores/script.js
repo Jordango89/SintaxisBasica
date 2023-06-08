9a59323d8287cf5ba3ee0f872a677fcf89ae40a3
Daniel Gómez <134562664+Jordango89@users.noreply.github.com>
2023-06-08 05:57:03 -0500

    cambio

--- 5-operadores/script.js
+++ 5-operadores/script.js
@@ -93,3 +93,22 @@ La variable ${a} es mayor o igual que ${b}: ${mayorIgual}
 La variable ${a} es menor o igual que ${b}: ${menorIgual}
 La variable ${a} es igual que ${b}: ${igual}
 La variable ${a} es distinta que ${b}: ${distinto}`)
+
+// 1. Saludar
+//   Pide el nombre en un mensaje de alerta
+let nombre = prompt("Por favor, ingresa tu nombre:");
+//   Muestra el nombre en un mensaje de alerta
+alert("Hola " + nombre + ", bienvenido/a!");
+let edad = prompt("Por favor ingrese su edad");
+//   Muestra en consola la edad
+console.log("Tienes " + edad + " años");
+// 2. Operaciones
+const PI = 3.14159; // Constante
+let radio = prompt ("Por favor ingresa el radio"); // Variable  
+let area = PI * radio ** 2; // Operador matemático y tipo de dato (número)
+let esMayorQueDiez = area > 10; // Operador de comparación y operador lógico
+let esMenorQueVeinte = area < 20; // Operador de comparación y operador lógico
+
+console.log("El área del círculo es: " + area); // Imprimir resultado en consola
+console.log("¿El área es mayor que 10? " + esMayorQueDiez); // Imprimir resultado en consola
+console.log("¿El área es menor que 20? " + esMenorQueVeinte); // Imprimir resultado en consola
